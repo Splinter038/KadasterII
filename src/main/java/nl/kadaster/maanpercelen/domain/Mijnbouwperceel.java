4777e9f2733ea27f11e0468b3fe1db247a8ef687
GrootheddeJeroen <jeroen.groothedde@kadaster.nl>
2019-05-27 15:17:39 +0200

    refactor winningsgebied

--- src/main/java/nl/kadaster/maanpercelen/domain/Mijnbouwperceel.java
+++ src/main/java/nl/kadaster/maanpercelen/domain/Mijnbouwperceel.java
@@ -2,7 +2,7 @@ package nl.kadaster.maanpercelen.domain;
 
 import nl.kadaster.maanpercelen.waardelijsten.Delfstof;
 
-public class Mijnbouwperceel extends Perceel{
+public class Mijnbouwperceel extends Winningsgebied{
 
     private Delfstof delfstof;
 }
